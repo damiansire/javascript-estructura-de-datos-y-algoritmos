7d974274eb4de095d483f04af0136ef29c75e41d
Damian Sire <damiansire@gmail.com>
2026-06-21 00:43:05 -0500

    fix(bst-stub-vacio): implementa insert, find, findMinNode e inOrder en el BST y exporta [manual]

--- Estructuras-de-datos/binary-search-tree/binary-search-tree.js
+++ Estructuras-de-datos/binary-search-tree/binary-search-tree.js
@@ -12,18 +12,64 @@ class BinarySearchTree {
         this.root = null
     }
 
+    insert(value) {
+        let node = new BinarySearchTreeNode(value);
+        if (this.root === null) {
+            this.root = node;
+            return node;
+        }
+        let current = this.root;
+        while (true) {
+            if (value < current.value) {
+                if (current.left === null) {
+                    current.left = node;
+                    return node;
+                }
+                current = current.left;
+            } else if (value > current.value) {
+                if (current.right === null) {
+                    current.right = node;
+                    return node;
+                }
+                current = current.right;
+            } else {
+                // valor duplicado: no se inserta
+                return current;
+            }
+        }
+    }
+
+    find(value) {
+        let current = this.root;
+        while (current !== null) {
+            if (value === current.value) {
+                return current;
+            }
+            current = value < current.value ? current.left : current.right;
+        }
+        return null;
+    }
+
+    findMinNode(node = this.root) {
+        if (node === null) { return null; }
+        while (node.left !== null) {
+            node = node.left;
+        }
+        return node;
+    }
+
+    getRootNode() {
+        return this.root;
+    }
+
+    inOrder(node = this.root, result = []) {
+        if (node !== null) {
+            this.inOrder(node.left, result);
+            result.push(node.value);
+            this.inOrder(node.right, result);
+        }
+        return result;
+    }
 }
 
-// find(value)
-// insert(data)
-// printInOrder(node)
-// printPreOrder(node)
-// printPostOrder(node)
-// remove(data)
-// findMinNode()
-// getRootNode()
-// search(node, data)
-// recursiveFindMinNode()
-// recursiveFind(value)
-// recursiveInsert(data, node)
-// specificInsert(valueParent, value)
+module.exports = { BinarySearchTree, BinarySearchTreeNode };
